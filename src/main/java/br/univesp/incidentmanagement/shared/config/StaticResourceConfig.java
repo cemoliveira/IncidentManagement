fb8c250072a15aa210cf7e8c46b5e5816489bdf7
Carlos Oliveira <cemoliveir@gmail.com>
2025-11-10 14:41:33 -0300

    Limpeza definitiva após o teste de funcionamento.

--- src/main/java/br/univesp/incidentmanagement/shared/config/StaticResourceConfig.java
+++ src/main/java/br/univesp/incidentmanagement/shared/config/StaticResourceConfig.java
@@ -1,15 +1,11 @@
 package br.univesp.incidentmanagement.shared.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class StaticResourceConfig implements WebMvcConfigurer {
-
-    /*@Value("${students.images.path}")
-    private String studentsImagesPath;*/
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
